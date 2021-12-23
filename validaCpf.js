4b50f48c5362a7e61d87b3c4077f09103733af07
kev1nnnnnnn <kevinbfv@gmail.com>
2021-12-23 10:57:42 -0400

    Validação de CPF com javascript utilizando metodos construtores e prototypes

---
+++ validaCpf.js
@@ -0,0 +1,51 @@
+function ValidaCPF(cpfEnviado) {
+
+    Object.defineProperty(this, 'cpfLimpo', {
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2); 
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    
+    return novoCpf === this.cpfLimpo;
+
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+    const total =  cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+    
+    const digito = 11 - (total % 11);
+    //se o digito for maior que 9, retorna 0, se nao, retorna o digito
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia =  function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCPF('658.353.380-40');
+
+if(cpf.valida()) {
+    console.log("CPF Válido: " + cpf.cpfLimpo);
+} else {
+    console.log("CPF Inválido!!");
+}
